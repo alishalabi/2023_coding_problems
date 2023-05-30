54dded8d06f1cbf5a56aaec20e9bac479bc5be0f
Ali Shalabi <ashalabi13@gmail.com>
2023-05-30 16:16:58 -0700

    Starting binary tree

---
+++ binary_search_tree.py
@@ -0,0 +1,15 @@
+"""
+Practice working with a binary search tree
+
+1. Build a binary search tree form sorted array
+"""
+
+
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+
+# WIP
